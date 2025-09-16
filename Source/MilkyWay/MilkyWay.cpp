39f69616411f35fea3ae19a1256f6be43ec3490d
Esphirian <paul.artige@gmail.com>
2025-09-16 16:39:08 +0200

    sources

---
+++ Source/MilkyWay/MilkyWay.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MilkyWay.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MilkyWay, "MilkyWay" );
